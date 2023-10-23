778192caa2caa95cd7f5afad8f973fb6a6b52fe7
Alisa | Side.one <alisa@side.one>
2023-10-23 15:56:49 +0800

    Update NavBar.js

--- src/app/layout/NavBar.js
+++ src/app/layout/NavBar.js
@@ -17,29 +17,31 @@ export default function NavBar() {
     <header
       data-aos="fade-down"
       data-aos-duration="2000"
-      className="flex items-center justify-center text-[#A1A1A1] relative h-[80px] space-x-[80px] z-50"
+      className="w-full h-[80px] z-50 fixed top-0 left-0 backdrop-blur-lg"
     >
-      <Image
-        src={sideLogo}
-        alt="side"
-        className="w-[120px] absolute left-[40px]"
-      />
-      {linkList.map((linkItem, index) => {
-        return (
-          <Link
-            key={index}
-            href={linkItem.url}
-            className={`${
-              linkItem.id === tab ? "text-[#63A2FF] navbar-select" : ""
-            }`}
-            onClick={() => {
-              setTab(linkItem.id);
-            }}
-          >
-            {linkItem.title}
-          </Link>
-        );
-      })}
+      <div className="backdrop-blur-lg w-full h-full flex items-center justify-center text-[#A1A1A1] relative space-x-[80px]">
+        <Image
+          src={sideLogo}
+          alt="side"
+          className="w-[120px] absolute left-[40px]"
+        />
+        {linkList.map((linkItem, index) => {
+          return (
+            <Link
+              key={index}
+              href={linkItem.url}
+              className={`${
+                linkItem.id === tab ? "text-[#63A2FF] navbar-select" : ""
+              }`}
+              onClick={() => {
+                setTab(linkItem.id);
+              }}
+            >
+              {linkItem.title}
+            </Link>
+          );
+        })}
+      </div>
     </header>
   );
 }
