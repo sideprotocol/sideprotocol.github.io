e5dd73af5ccd295908820bc1d7657199c9f60ca2
Alisa | Side.one <alisa@side.one>
2023-10-23 11:38:27 +0800

    Update Footer.js

--- src/app/layout/Footer.js
+++ src/app/layout/Footer.js
@@ -121,93 +121,95 @@ export default function Footer() {
   ];
   return (
     <section className="">
-      <div className="">
-        <Image src={sidelogo} className="w-[160px]" alt="side" />
-      </div>
-      <div className="flex">
-        {/* left */}
-        <div>
-          <div className="mt-16">
-            <div className="text-sm font-light mb-3">
-              Sign up for newsletter
-            </div>
-            <div className="w-[380px]">
-              <form action="https://submit-form.com/bvras8Ee">
-                <input
-                  type="email"
-                  placeholder="Enter your email"
-                  className="placeholder:text-[#ffffff66] text-white bg-[#03070E] border-white form-input rounded w-full"
-                  required
-                />
-              </form>
-            </div>
-          </div>
-          <div className="flex my-8 gap-7">
-            <a
-              className="py-[10px] block"
-              href="https://discord.com/invite/GN99earcFR"
-              target="_blank"
-            >
-              <Image src={sidediscord} className="w-9" alt="side" />
-            </a>
-            <a
-              className="py-[10px] block"
-              href="https://twitter.com/SideProtocol"
-              target="_blank"
-            >
-              <Image src={sidegitbook} className="w-9" alt="side" />
-            </a>
-            <a
-              className="py-[10px] block"
-              href="https://github.com/sideprotocol"
-              target="_blank"
-            >
-              <Image src={sidegithub} className="w-9" alt="side" />
-            </a>
-            <a
-              className="py-[10px] block"
-              href="https://github.com/sideprotocol"
-              target="_blank"
-            >
-              <Image src={sidemedium} className="w-9" alt="side" />
-            </a>
-            <a
-              className="py-[10px] block"
-              href="https://github.com/sideprotocol"
-              target="_blank"
-            >
-              <Image src={sidetwitter} className="w-9" alt="side" />
-            </a>
-          </div>
-          <div className="text-[#848895] font-light">
-            © 2023 Side Labs All Rights Reserved
-          </div>
+      <div className="w-full h-full flex flex-col justify-center mx-14">
+        <div className="">
+          <Image src={sidelogo} className="w-[160px]" alt="side" />
         </div>
-        {/* right */}
-        <ul className="flex w-full">
-          {footMenuList.map((item, index) => {
-            return (
-              <li
-                key={index}
-                className="flex flex-col items-start justify-start w-1/5 mr-10"
+        <div className="flex">
+          {/* left */}
+          <div>
+            <div className="mt-16">
+              <div className="text-sm font-light mb-3">
+                Sign up for newsletter
+              </div>
+              <div className="w-[380px]">
+                <form action="https://submit-form.com/bvras8Ee">
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    className="placeholder:text-[#ffffff66] text-white bg-[#03070E] border-white form-input rounded w-full"
+                    required
+                  />
+                </form>
+              </div>
+            </div>
+            <div className="flex my-8 gap-7">
+              <a
+                className="py-[10px] block"
+                href="https://discord.com/invite/GN99earcFR"
+                target="_blank"
               >
-                <div className="uppercase text-base mb-7">{item.title}</div>
-                {item.child.map((child, childIndex) => {
-                  return (
-                    <a
-                      key={childIndex}
-                      className="mb-7 text-[#848895] capitalize hover:text-[#8AFBB7] font-light text-sm"
-                      href={child.href}
-                      target="_blank"
-                    >
-                      {child.name}
-                    </a>
-                  );
-                })}
-              </li>
-            );
-          })}
-        </ul>
+                <Image src={sidediscord} className="w-9" alt="side" />
+              </a>
+              <a
+                className="py-[10px] block"
+                href="https://twitter.com/SideProtocol"
+                target="_blank"
+              >
+                <Image src={sidegitbook} className="w-9" alt="side" />
+              </a>
+              <a
+                className="py-[10px] block"
+                href="https://github.com/sideprotocol"
+                target="_blank"
+              >
+                <Image src={sidegithub} className="w-9" alt="side" />
+              </a>
+              <a
+                className="py-[10px] block"
+                href="https://github.com/sideprotocol"
+                target="_blank"
+              >
+                <Image src={sidemedium} className="w-9" alt="side" />
+              </a>
+              <a
+                className="py-[10px] block"
+                href="https://github.com/sideprotocol"
+                target="_blank"
+              >
+                <Image src={sidetwitter} className="w-9" alt="side" />
+              </a>
+            </div>
+            <div className="text-[#848895] font-light">
+              © 2023 Side Labs All Rights Reserved
+            </div>
+          </div>
+          {/* right */}
+          <ul className="flex w-full ml-20">
+            {footMenuList.map((item, index) => {
+              return (
+                <li
+                  key={index}
+                  className="flex flex-col items-start justify-start w-1/5 mr-10"
+                >
+                  <div className="uppercase text-base mb-7">{item.title}</div>
+                  {item.child.map((child, childIndex) => {
+                    return (
+                      <a
+                        key={childIndex}
+                        className="mb-7 text-[#848895] capitalize hover:text-[#8AFBB7] font-light text-sm"
+                        href={child.href}
+                        target="_blank"
+                      >
+                        {child.name}
+                      </a>
+                    );
+                  })}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </section>
   );
